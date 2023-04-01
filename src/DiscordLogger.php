cc676550f91ac3510de0c2125af12186947430cb
Dimitar Yanakiev <dyanakiev@users.noreply.github.com>
2023-04-01 22:58:34 +0300

    Improve logging #2

--- src/DiscordLogger.php
+++ src/DiscordLogger.php
@@ -6,21 +6,21 @@ use Monolog\Logger;
 
 class DiscordLogger
 {
-	/**
-	 * Create a custom Monolog instance.
-	 *
-	 * @param  array $config
-	 * @return \Monolog\Logger
-	 */
-	public function __invoke(array $config)
-	{
-		$log = new Logger('discord');
+    /**
+     * Create a custom Monolog instance.
+     *
+     * @param array $config
+     * @return \Monolog\Logger
+     */
+    public function __invoke(array $config)
+    {
+        $log = new Logger('discord');
 
-		if (app()->environment($config['environment'] ?? 'production')) {
-			$log->pushHandler(new DiscordHandler($config['webhook'], config('app.name'), null, $config['level'] ?? 'DEBUG', true, $config['role_id'] ?? null));
-		}
+        if (app()->environment($config['environment'] ?? 'production')) {
+            $log->pushHandler(new DiscordHandler($config));
+        }
 
-		return $log;
-	}
+        return $log;
+    }
 }
 
